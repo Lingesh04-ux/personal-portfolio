434d2a00cb888e36bdbb86973af40b142901b29b
Lingesh Hari <lingeshhari4@gmail.com>
2025-05-01 18:07:10 +0530

    problem cleared in MainBody4.jsx

--- src/components/MainBody4.jsx
+++ src/components/MainBody4.jsx
@@ -1,5 +1,5 @@
 import React,{ useContext } from 'react';
-import '../style/MainBody4'
+import '../style/MainBody4.scss';
 import experience from '../assets/experienceBanner/1.png';
 import mobileEx from '../assets/experienceBanner/mob1.png';
 import { ScrollContext } from '../functions/ScrollContext';
